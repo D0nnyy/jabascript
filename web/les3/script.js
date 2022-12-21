9490e25c23423b1af67c270f0a664d74a2bf1a61
D0nnyy <34960@ma-web.nl>
2022-12-21 14:04:08 +0100

    message

---
+++ web/les3/script.js
@@ -0,0 +1,13 @@
+const myImage = document.getElementById("myImage")
+
+function changeImage(){
+    myImage.src = "img/plaatje1.jpg"
+}
+
+function changeImage2(){
+    myImage.src = "img/plaatje2.jpg"
+}
+
+function changeImage3(){
+    myImage.src = "img/plaatje3.jpg"
+}
